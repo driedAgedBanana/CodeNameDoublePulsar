c9f5d17d1f88f49a889332f0743d4e985ca515ae
driedAgedBanana <127958189+driedAgedBanana@users.noreply.github.com>
2025-11-22 17:53:59 +0100

    Expanding level 1
    Adding toxins

--- Double_Pulsar/Assets/_Scripts/Enviroment/Damageable.cs
+++ Double_Pulsar/Assets/_Scripts/Enviroment/Damageable.cs
@@ -1,14 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class Damageable : MonoBehaviour
 {
     public int damageAmount;
+
+    private Coroutine damageCoroutine;
+    [SerializeField] private float _damageInterval = 1f;
+    private bool _isEnteringTriggerZone = false;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.TryGetComponent<PlayerHealth>(out PlayerHealth playerHealth))
         {
             playerHealth.TakeDamage(damageAmount, transform.position);
             Debug.Log("Player took 10 damage from DamageTest.");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _isEnteringTriggerZone = true;
+        damageCoroutine = StartCoroutine(DealDamageOverTime(collision.GetComponent<PlayerHealth>()));
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _isEnteringTriggerZone = false;
+        StopCoroutine(damageCoroutine);
+    }
+
+    private IEnumerator DealDamageOverTime(PlayerHealth playerHealth)
+    {
+        while (_isEnteringTriggerZone)
+        {
+            playerHealth.TakeDamage(damageAmount, transform.position);
+            yield return new WaitForSeconds(_damageInterval);
         }
     }
 }
